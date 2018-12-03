d5b4705a29e5d4a502cbc7dda56cec51f7f2c312
AlfCampelo <39158224+AlfCampelo@users.noreply.github.com>
2018-12-03 14:39:50 +0100

    "commit desde github"

---
+++ src/pruebacollection/PruebaCollection.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacollection;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alfredo Campelo Guerra
+ */
+public class PruebaCollection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Collection<String> listaMarcaChoches = new ArrayList();
+
+        listaMarcaChoches.add("Porsche");
+        listaMarcaChoches.add("Audi");
+        listaMarcaChoches.add("Mercedes");
+        listaMarcaChoches.add("Seat");
+        listaMarcaChoches.add("Renault");
+        listaMarcaChoches.add("Honda");
+
+        System.out.println("Número de elementos antes de eliminar: " + listaMarcaChoches.size());
+        System.out.println(listaMarcaChoches.toString());
+
+        listaMarcaChoches.remove("Seat");
+        listaMarcaChoches.remove("Mercedes");
+
+        System.out.println("Número de elementos despues de eliminar Seat y Mercedes: "
+                + listaMarcaChoches.size());
+        System.out.println(listaMarcaChoches.toString());
+
+    }
+
+}
